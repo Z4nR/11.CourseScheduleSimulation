e9876b81b7cbc50190f5290e87ae38d72438d2d0
Zulham Ari <ari.comunity1@gmail.com>
2021-07-18 09:47:48 +0700

    finish TODO 6 and 7

--- app/src/main/java/com/dicoding/courseschedule/paging/CourseViewHolder.kt
+++ app/src/main/java/com/dicoding/courseschedule/paging/CourseViewHolder.kt
@@ -1,6 +1,7 @@
 package com.dicoding.courseschedule.paging
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.courseschedule.R
 import com.dicoding.courseschedule.data.Course
@@ -10,6 +11,9 @@ class CourseViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
     private lateinit var course: Course
     private val timeString = itemView.context.resources.getString(R.string.time_format)
+    private val tvTime : TextView = view.findViewById(R.id.tv_time)
+    private val tvLecture : TextView = view.findViewById(R.id.tv_lecturer)
+    private val tvCourse : TextView = view.findViewById(R.id.tv_course)
 
     //TODO 7 : Complete ViewHolder to show item
     fun bind(course: Course, clickListener: (Course) -> Unit) {
@@ -18,9 +22,11 @@ class CourseViewHolder(view: View): RecyclerView.ViewHolder(view) {
         course.apply {
             val dayName = getByNumber(day)
             val timeFormat = String.format(timeString, dayName, startTime, endTime)
-
-
+            tvTime.text = timeFormat
         }
+
+        tvLecture.text = course.lecturer
+        tvCourse.text = course.courseName
 
         itemView.setOnClickListener {
             clickListener(course)
